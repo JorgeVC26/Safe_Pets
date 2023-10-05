d9f1fc8d877151c0363109a6a481193fd04d1e11
Kerlin-C <kerlin.carranza.solano@est.una.ac.cr>
2023-10-05 17:54:02 -0600

    cosas minimas

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import './Style/style.css';
+
 
 function Home() {
   return (
@@ -8,13 +10,33 @@ function Home() {
     
         <h1>Safe<span>Pets</span></h1>
     
-    </header>
-    
-    <div>
-        <h1></h1>
-        <Link to="./Service">Servicio</Link>
-        
-      </div>
+        </header>
+    <div style={{display: 'flex'}}>
+          <div className='gasto sombra' style={{width: '30%', height:'40rem', justifyContent: 'center'}}>
+            <div className='contenido-gasto'>
+              <Link to="./Service" className='Titulos'>Servicios</Link>
+              
+            
+
+            </div>
+          </div>
+          <div className='gasto sombra margenHorizontal' style={{width: '30%', height:'40rem', justifyContent: 'center'}}>
+            <div className='contenido-gasto'>
+              <Link to="./Service" className='Titulos'>Usuarios</Link>
+              
+            
+
+            </div>
+          </div>
+          <div className='gasto sombra' style={{width: '30%', height:'40rem', justifyContent: 'center'}}>
+            <div className='contenido-gasto'>
+              <Link to="./Service" className='Titulos'>Más</Link>
+              
+            
+
+            </div>
+          </div>
+</div>
       </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import ControlServicios from './ControlServicios'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ControlServicios from './ControlServicios';
 
-const Header = (servicios, servicio) => {
+const Header = ({ servicios, servicio }) => {
   return (
-  <>
-    <header>
-    
-      <h1>Safe<span>Pets</span></h1>
-    
-        <ControlServicios 
-        servicios = {servicios}
-        />
-    </header>
-</>
-  )
-}
+    <>
+      <header>
+        <div>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            <h1>Safe<span>Pets</span></h1>
+          </Link>
+        </div>
+        <ControlServicios servicios={servicios} />
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
