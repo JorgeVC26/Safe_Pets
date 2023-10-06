d1070c90dd76632fda3bec5cc232b22d5f0a239e
mariahrdz <maria.hernandez.perez@est.una.ac.cr>
2023-10-06 11:36:18 -0600

    Cambios Historia

--- src/Historia.jsx
+++ src/Historia.jsx
@@ -1,8 +1,23 @@
 import { Link } from "react-router-dom";
-import './Style/style.css';
-import mascotas from './img/mascotas.jpg'
+import './Style/historia_carrousel.css';
 
-function Historia() {
+
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css'; 
+import 'slick-carousel/slick/slick-theme.css';
+import image1 from './img/imagen.png'; 
+import image2 from './img/img.jpg';
+import image3 from './img/img.jpg';
+
+function Home() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
+  
     return (
         <div>
           {/* */}
@@ -10,28 +25,34 @@ function Historia() {
           <h1>Safe<span>Pets</span></h1>
             </header>
 
-
-          <div className="secciones-principales">
-      <div style={{display: 'flex'}}>
-            
-            
-            <div className='seccion sombra margenHorizontal' style={{width: '80%', height:'70rem', justifyContent: 'center'}}>
-              <div className='contenido-gasto'  style={{flexDirection: 'column'}}>
-                <h1>Historia</h1>
-                <p>SAFEPETS surgió debido al estilo de vida actual y la creciente dependencia de la tecnologia han, el cual han generado un desafio en asegurarnos de que nuestras mascotas reciban la atención necesaria en nuestra ausencia Por ende nos planteamos la necesidad de encontrar soluciones innovadoras que combinen tecnologia y cuidado animal para garantizar que nuestras queridas mascotas estén bien atendidas</p>
-              </div>
-              <div>
-              <img src={mascotas} alt="imagen de comentarios" style={{height: '60rem'}}/>
-
-              </div>
-              
+      <div className="card">
+        <div className="card-content">
+          <div className="card-text">
+            <h2 className="card-title">Nuestra Historia</h2>
+            <p className="card-description">SAFEPETS surgió debido al estilo de vida actual 
+            y la creciente dependencia de la tecnología, lo cual ha generado un desafío en 
+            asegurarnos de que nuestras mascotas reciban la atención necesaria en nuestra ausencia. 
+            Por ende, nos planteamos la necesidad de encontrar soluciones innovadoras que combinen 
+            tecnología y cuidado animal para garantizar que nuestras queridas mascotas estén bien atendidas.</p>
+          </div>
+        </div>
+        <div className="card-image">
+          <Slider {...settings}>
+            <div>
+              <img src={image1} alt="Imagen de mascota 1" className="pet-image" />
             </div>
-            
-  </div>
+            <div>
+              <img src={image2} alt="Imagen de mascota 2" className="pet-image" />
+            </div>
+            <div>
+              <img src={image3} alt="Imagen de mascota 3" className="pet-image" />
+            </div>
+          </Slider>
         </div>
-        </div>
+        
+      </div>
+      </div>
     );
   }
   
-  
-  export default Historia
+  export default Home;
